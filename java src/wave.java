fb069c6edf6977066135cf2b2af615f04c934d22
Dr_Coomer <54870332+yoshisaac@users.noreply.github.com>
2023-05-30 04:20:51 -0400

    more customization

--- java src/wave.java
+++ java src/wave.java
@@ -6,15 +6,28 @@ class wave
 
     static Scanner GetInput = new Scanner(System.in);
     
-    static int WaveLength, Wavetime;
+    static int WaveLengthForward, WaveLengthBack, Wavetime;
     static String Character;
+
+	final static char space = ' ';
 
 	public static void main(String[] args) throws IOException, InterruptedException 
 	{
-            System.out.print("Wave Length: ");
-            WaveLength = GetInput.nextInt();
+            System.out.print("Wave Length Forward: ");
+            WaveLengthForward = GetInput.nextInt();
 
             System.out.print('\n');
+
+			System.out.print("Wave Length Back: ");
+			WaveLengthBack = GetInput.nextInt();
+
+			if (WaveLengthBack > WaveLengthForward)
+			{
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				return;
+			}
+
+			System.out.print('\n');
             
             System.out.print("Wait per draw in ms: ");
             Wavetime = GetInput.nextInt();
@@ -29,24 +42,24 @@ class wave
             
 		while (true)
 		{
-			for (int i = 0; i < WaveLength; i++)
+			for (int spaces = WaveLengthBack; spaces < WaveLengthForward; spaces++)
 			{
-				spaces(i);
-				System.out.print(Character + '\n');
+				spaces(spaces, space);
+				System.out.println(Character);
 				Thread.sleep(Wavetime);
 			}
 
-			for (int x = WaveLength; x > 0; x--)
+			for (int spaces = WaveLengthForward; spaces > WaveLengthBack; spaces--)
 			{
-				spaces(x);
-				System.out.print(Character + '\n');
+				spaces(spaces, space);
+				System.out.println(Character);
 				Thread.sleep(Wavetime);
 			}
 		}
 	}
 
-	public static void spaces(int spaces)
+	public static void spaces(int NumberOfCharacters, char character)
 	{
-		for (int c = 0; c < spaces; c++) { System.out.print(" "); }
+		for (int c = 0; c < NumberOfCharacters; c++) { System.out.print(character); }
 	}
 }
