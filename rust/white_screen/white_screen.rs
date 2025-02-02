29db023b692fddf6c3b3f41eed996c448875634f
Dr_Coomer <isfortner@gmail.com>
2025-02-02 18:50:47 -0500

    Update white_screen

--- rust/white_screen/white_screen.rs
+++ rust/white_screen/white_screen.rs
@@ -1,17 +1,41 @@
 use std::time;
 use term_size;
+use std::io;
+use std::io::Write;
+use ctrlc;
+use std::num::ParseIntError;
 
 fn main() {
-    let Some((columns, lines)) = term_size::dimensions() else { todo!() };
-    let mut mono_characters: String = String::new();
-    let mut x: usize = 0;
-    while x < columns {
-	mono_characters += "█";
-	x+=1;
-    }
-    loop {
-	println!("{}", mono_characters);
+
+    ctrlc::set_handler(move || {
+        print!("\u{001B}[?25h");
+	std::process::exit(0);
+    }).expect("Error setting Ctrl-C handler.");
 	
+    print!("\u{001B}[?25l");
+
+    let Some((columns_init, lines_init)) = term_size::dimensions() else { todo!() };
+    let mut mono_characters_init: String = String::new();    
+
+    for _y in 0..lines_init {
+	for _x in 0..columns_init {
+	    mono_characters_init += "█";
+	}
+    }
+
+    println!("{}", mono_characters_init);
+    
+    loop {
+	let Some((columns, _lines)) = term_size::dimensions() else { todo!() };
+	let mut mono_characters: String = String::new();
+
+	for _ in 0..columns {
+	    mono_characters += "█";
+	}
+
+	print!("{}", mono_characters);
+	let _ = io::stdout().flush();
 	std::thread::sleep(time::Duration::from_nanos(99999999));
+	print!("\n");
     }
 }
